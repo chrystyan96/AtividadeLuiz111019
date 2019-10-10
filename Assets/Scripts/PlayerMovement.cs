c532d731c0d13f147a0b042b22dca125832fe581
Chrystyan Byrnes <chrystyan96@hotmail.com>
2019-10-09 23:15:53 -0300

    first commit

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    private PlayerController controller;
+	public float runSpeed = 40f;
+	float horizontalMove = 0f;
+    float verticalMove = 0f;
+	bool jump = false;
+
+    void Start() {
+        controller = GetComponent<PlayerController>();
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		horizontalMove = Input.GetAxisRaw("Horizontal") * runSpeed;
+
+        controller.GetAnimator().SetFloat("xSpeed", Mathf.Abs(horizontalMove));
+
+		if (Input.GetButtonDown("Jump"))
+		{
+            jump = true;
+            controller.GetAnimator().SetBool("isJumping", true);
+		}
+	}
+
+	void FixedUpdate ()
+	{
+		// Move our character
+		controller.Move(horizontalMove * Time.fixedDeltaTime, jump);
+	}
+
+    public void OnLanding() 
+    {
+        jump = false;
+        controller.GetAnimator().SetBool("isJumping", false);
+    }
+}
